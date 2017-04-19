5ab662873dd0dc3b082cbd44fd28b7ab83fc758b
Benedikt Müssig <benedikt.muessig@gmail.com>
2017-04-19 20:05:21 +0200

    More work on various utilities

--- utils.c
+++ utils.c
@@ -1,13 +1,94 @@
 #include "utils.h"
 
-int freadint(int* val, FILE* file)
+int fbavail(FILE* stream)
 {
-  return fread(val, sizeof(int), 1, file) > 0;
+  int num, res;
+  if(!stream)
+    return -1;
+  if((res = ioctl(fileno(stream), I_NREAD, &num)) == 0)
+    return num;
+  else
+    return -1;
 }
 
-int freaduint(unsigned int* val, FILE* file)
+int fflushi(FILE* stream)
 {
-  return fread(val, sizeof(unsigned int), 1, file) > 0;
+  int num, fp;
+  if(!stream)
+    return false;
+  if((fp = fileno(stream)) != 0)
+    return false;
+  while(num > 0) {
+    if(ioctl(fp, I_NREAD, &num) != 0)
+      return false;
+    if(fgetc(stream) == EOF)
+      break;
+  }
+  return true;
+}
+
+int fwritebyte(unsigned char value, FILE* file)
+{
+  return fwrite(&value, sizeof(unsigned int), 1, file);
+}
+
+int fwriteint(int value, FILE* file)
+{
+  return fwrite(&value, sizeof(int), 1, file);
+}
+
+int fwriteuint(unsigned int value, FILE* file)
+{
+  return fwrite(&value, sizeof(unsigned int), 1, file);
+}
+
+int freadbyte(unsigned char* value, unsigned int timeout, FILE* file)
+{
+  return freadt(value, sizeof(unsigned char), 1, timeout, file);
+}
+
+int freadint(int* value, unsigned int timeout, FILE* file)
+{
+  return freadt(value, sizeof(int), 1, timeout, file);
+}
+
+int freaduint(unsigned int* value, unsigned int timeout, FILE* file)
+{
+  return freadt(value, sizeof(unsigned int), 1, timeout, file);
+}
+
+size_t freadt(void* ptr, size_t size, size_t count, unsigned int timeout, FILE* stream)
+{
+  if(timeout) {
+    usecs start = tmr_start();
+    int timedOut;
+
+    // Pause program execution while the bytes arrive, but max. for timeout microsecs
+    while(!(timedOut = tmr_check(start) >= timeout) && fbavail(stream) < size * count);
+
+    if(timedOut) {
+      errno = ETIME;
+      return 0;
+    }
+  }
+
+  return fread(ptr, size, count, stream);
+}
+
+int fsetblocking(int isBlocking, FILE* file)
+{
+  int fd, flags;
+  if(!file)
+    return false;
+  if((fd = fileno(file)) == -1)
+    return false;
+  flags = fcntl(fd, F_GETFL, 0);
+  if(isBlocking)
+    flags &= ~O_NONBLOCK;
+  else
+    flags |= O_NONBLOCK;
+
+  return (fcntl(fd, F_SETFL, flags) == 0);
 }
 
 // http://stackoverflow.com/a/5820991
--- utils.h
+++ utils.h
@@ -4,10 +4,23 @@
 #include <string.h>
 #include <ctype.h>
 #include <stdio.h>
+#include <stropts.h>
+#include <stdbool.h>
+#include <errno.h>
+#include <unistd.h>
+#include <fcntl.h>
 #include <sys/time.h>
 
-int freadint(int* val, FILE* file);
-int freaduint(unsigned int* val, FILE* file);
+int fbavail(FILE* stream);
+int fflushi(FILE* stream);
+int fwritebyte(unsigned char value, FILE* file);
+int fwriteint(int value, FILE* file);
+int fwriteuint(unsigned int value, FILE* file);
+int freadbyte(unsigned char* value, unsigned int timeout, FILE* file);
+int freaduint(unsigned int* value, unsigned int timeout, FILE* file);
+int freadint(int* value, unsigned int timeout, FILE* file);
+size_t freadt(void* ptr, size_t size, size_t count, unsigned int timeout, FILE* stream);
+int fsetblocking(int isBlocking, FILE* file);
 
 int strcicmp(char const *a, char const *b);
 unsigned int crc32(unsigned char *data, unsigned int size);
