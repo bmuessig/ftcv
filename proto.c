020e20fa489e8bd554610273d38badd1cbca0417
Benedikt Müssig <benedikt.muessig@gmail.com>
2017-04-19 20:05:35 +0200

    Worked on the protocol

---
+++ proto.c
@@ -0,0 +1,10 @@
+#include "proto.h"
+
+int awaitResponse(unsigned int timeout)
+{
+  unsigned char resp;
+  if(freadbyte(&resp, timeout) >= 0)
+    return resp; // We got a valid response
+  else
+    return -1; // We had an error or a timeout
+}
--- proto.h
+++ proto.h
@@ -6,10 +6,14 @@
 
 // Start of the entire transmission
 #define FTCV_PROTO_SOH 0x01
-// Successfull end of the current data block, followed by CRC32
+// Start of a data block, followed by the size in bytes used
 #define FTCV_PROTO_STX 0x02
-// Successfull end of the whole transmission
+// Successfull end of the current data block, followed by CRC32
 #define FTCV_PROTO_ETX 0x03
+// Successfull end of the whole transmission
+#define FTCV_PROTO_EOT 0x04
+// End of data block, needs an ack to continiue
+#define FTCV_PROTO_ETB 0x17
 // Acknowledge
 #define FTCV_PROTO_ACK 0x06
 // Not Acknowledge (e.g. CRC32 missmatch)
@@ -18,5 +22,14 @@
 #define FTCV_PROTO_SYN 0x16
 // Cancel the entire transmission (client to server)
 #define FTCV_PROTO_CAN 0x18
+// Data escape sequence
+// 0x10 0x10 referrs the the data byte 0x10. (server to client)
+// 0x10 0x02 definitively marks the end of block. (server to client)
+// 0x10 0x16 requests the current offset in the block (client to server)
+// 0x10 0x18 cancels the entire transmission (client to server)
+// All other sequences are undefined and are skipped
+#define FTCV_PROTO_DLE 0x10;
+
+int awaitAck(unsigned int timeout);
 
 #endif
